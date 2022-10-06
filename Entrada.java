bdf46beb33b9d5a6a525652555d5320a8cd3bc68
guilhermebraga6926 <guilhermebraga1626@gmail.com>
2022-10-06 01:10:14 -0300

    pull

--- Entrada.java
+++ Entrada.java
@@ -5,20 +5,27 @@ public class Entrada {
 
     public static void main(String[] args)
     {
-        Scanner entrada = new Scanner(System.in);
+        try (Scanner entrada = new Scanner(System.in)) {
+            int idade;
+            int ano_atual;
+            int ano_nascimento;
+            
+            //pergunta a idade e armazena 
+            System.out.println("Digite sua idade:");
+            
+            idade = entrada.nextInt(); // Lê a idade
+            
+            //Cria um objeto do tipo calendario e armazena o ano atual
 
-        int idade;
-        int ano_atual;
-        int nascimento;
-        
-        //pergunta a idade e armazena 
-        System.out.println("Digite sua idade:");idade = entrada.nextint();
-        
-        
+            Calendar calendario = Calendar.getInstance(); ano_atual= calendario.get(Calendar.YEAR);
+
+            ano_nascimento = ano_atual - idade ;
+
+            
+            //Imprime na tela o ano que a pessoa nasceu
+            System.out.printf("Voce nasceu em " + ano_nascimento + "\n");
+        }
     
     
     }
-
-
-
 }
